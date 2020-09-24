b7c4c9ac65864f5d5d6688de6df64334eb16c7d8
clairebogdan <bogdanclaire@gmail.com>
2020-09-24 16:12:56 -0400

    updated Bogdan Reeds page

--- src/components/pages/BogdanReeds.js
+++ src/components/pages/BogdanReeds.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ProjectNav from '../navbar/ProjectNav';
 import '../styles/ProjectItems.css'
+import reeds from '../../assets/reeds.jpg'
 
 function BogdanReeds() {
 
@@ -14,12 +15,19 @@ function BogdanReeds() {
         </div>
 
         <div className='project-img'>
-            image goes here
+            <img src={reeds} width='100%' height='100%'/>
         </div>
 
         <div className='description-box'>
             <h1><center>Description:</center></h1>
-            <p>this is where the description will go</p>
+            <p>This project allows the user to "order" oboe reeds from my 
+                company, Bogdan Reeds. As of right now, this is a prototype application.
+                The user can interact with the CLI and answer questions regarding their 
+                oboe reed order. The program collects the user's information such as 
+                name, address, payment method, number of reeds, and shipping option.
+                <br/><br/>
+                This program will be expanded to become a website and web application.
+            </p>
         </div>
 
         <div className='tools-links'>
