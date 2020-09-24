baa02627eb1c696eac54a164329d2087c94e13de
clairebogdan <bogdanclaire@gmail.com>
2020-09-24 16:07:48 -0400

    Instrument Calc page updated

--- src/components/pages/InstrumentCalc.js
+++ src/components/pages/InstrumentCalc.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import ProjectNav from '../navbar/ProjectNav';
-import '../styles/ProjectItems.css'
+import '../styles/ProjectItems.css';
+import ICmain from '../../assets/project_pics/instrumentcalc/ICmain.png'
 
 function InstrumentCalc() {
 
@@ -9,17 +9,35 @@ function InstrumentCalc() {
         <>
         <body>
         <div className='project-top'>
-            <h1>Beginning Band Instrumentation Calculator</h1>
+            <h1>Beginner Band Instrumentation Calculator</h1>
             <h2>Web Application</h2>
         </div>
 
         <div className='project-img'>
-            image goes here
+            <img src={ICmain} width='100%' height='100%'/>
         </div>
 
         <div className='description-box'>
             <h1><center>Description:</center></h1>
-            <p>this is where the description will go</p>
+            <p>Back when I was a band director, I remember constantly having to recalculate 
+                my beginning band numbers. The instrumentation of the beginning band is integral
+                for achieving a balanced sound in the band program.
+                Planning for the instrumentation was difficult due to the constantly changing
+                beginning band numbers. My principal would tell me that I would have 
+                70 new beginners one week, then 170 the next week!
+                <br/><br/>
+                This app is designed to help band directors quickly calculate the 
+                instrumentation of the beginner band. 
+                There are 3 options: 
+                <br/><br/>
+                <strong>Full instrumentation</strong> - uses Scott Rush's "Habits of a Middle School Band Director"  
+                instrumentation percentages
+                <br/>
+                <strong>Reduced instrumentation</strong> - does not include 
+                specialty instruments
+                <br/>
+                <strong>Categorical instrumentation</strong> - only includes general instrument categories
+                </p>
         </div>
 
 
