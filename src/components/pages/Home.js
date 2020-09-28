955a826a350aa16c584d21da76d1b598a1c02c9b
clairebogdan <bogdanclaire@gmail.com>
2020-09-28 15:04:09 -0400

    fixed mobile contact.js and home page overlay div

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,25 @@ import React from 'react';
 import '../../App.css';
 import '../styles/Home.css'
 import boonie from '../../assets/boonie.jpg'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <>
-        <h1 className='home'>Hi, I'm Claire Bogdan</h1>
         <div className='headshot'>
-            <img src={boonie} alt='dog'/>
+            <div className='tag'>
+                <h1>Hi, i'm Claire!</h1>
+                <br/>
+                <h2>Software engineer,<br/>
+                    Former teacher,<br/>
+                    Musician
+                </h2>
+                <br/>
+                <Link to='/about' className='learn-more-btn'>
+                    Learn more <i class="fas fa-chevron-right"></i>
+                </Link>
+            </div>
+            <img src={boonie} alt='dog' width='100%' height='100%'/>
         </div>
         </>
     );
