cf00bad9e53322060c26070ba565dc6e160b5c1f
clairebogdan <bogdanclaire@gmail.com>
2020-09-28 19:29:57 -0400

    about description updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,29 +19,57 @@ export default function About() {
                 <img src={computer} alt="computer" width="100%" height="100%"/>
             </div>
             <div className='bio-text'>
-                <p>I am a recent computer science graduate from Western Governors University (September 2020).
-                This is just one aspect of my career change journey.</p>
+                <h2>Hi, I'm Claire Bogdan! I am a recent computer science graduate from 
+                Western Governors University (September 2020). I am actively searching for 
+                software engineering opportunities in the Columbus, Ohio area. </h2>
             </div>
+        </div>
 
+        <div className='bio-container'>
+            <p>Over the past year, I have completed 10 software engineering projects and obtained 
+                4 certifications. These projects consist of assignments for my degree program as well 
+                as personal projects (including this website). One of my web applications, PerfectFit, received the 
+                Capstone Certificate of Excellence from WGU for its exemplary work.
+                <br/><br/>
+                Most of my work is written in either Python or Java. I have made web applications, command-line 
+                applications, stand-alone applications, and dabbled in AI by creating a chatbot and a robot simulation. 
+                Currently, I am working on improving my JavaScript skills. I always enjoy learning new things!
+            </p>
+            <div className='center'>
+                <Link to='/portfolio' className='about-btn'>
+                        See Portfolio <i class="fas fa-chevron-right"></i>
+                </Link>
+            </div>
+        </div>
+
+        <div className='bio'>
             <div className='bio-text'>
-                <p>In June 2019, I decided to change careers. I originally went to the University of South Carolina (UofSC)
-                to pursue a Bachelor's in Music Education, with a Performance Certificate in Oboe. While at UofSC, 
-                I was the principal oboist of the Wind Ensemble as well as the Drum Major for the marching band
-                (the Carolina Band). Upon graduation in May 2017, I went on to be a public middle school band director for
-                two years-- one year in Parkland, Florida, and another in Summerville, South Carolina.
-                During my first year teaching, I expereinced a school shooting. This was the main catalyst for 
-                deciding to leave teaching. I still loved teaching and performing, but I knew I needed to do 
-                something else as my career.</p>
+                <h2>My career change story...</h2><br/>
+                <p>I started playing the oboe in the 6th grade. I loved playing the oboe and being in band. 
+                    I continued to play in high school, participating in state and national level performing 
+                    ensembles. Because of my passion in music, I decided to major in Music Education with a 
+                    Performance Certificate in Oboe at the University of South Carolina (UofSC). While at UofSC, I was the principal 
+                    oboist of the Wind Ensemble as well as the Drum Major for the marching band (the Carolina Band). 
+                    Upon graduation in May 2017, I went on to be a public middle school band director for 
+                    two years -- one year in Parkland, Florida, and another in Summerville, South Carolina.
+                    </p>
             </div>
             <div className='bio-pic'>
-                <img src={plaques} alt="SC awards" width="100%" height="100%"/>
+                <img src={oboe2} alt="SC awards" width="100%" height="100%"/>
             </div>
 
             <div className='bio-pic'>
-                <img src={oboe2} alt="me with oboe" width="100%" height="100%"/>
+                <img src={plaques} alt="me with oboe" width="100%" height="100%"/>
             </div>
             <div className='bio-text'>
-                <p>stuff!!!!</p>
+                <p>During my first year teaching, I expereinced a school shooting. This was the main catalyst for 
+                    deciding to leave teaching. I still loved teaching and performing, but I knew I needed to do 
+                    something else as my career. This was when I was encouraged to try coding. I went to codecademy.com 
+                    and followed along with one of their Java tutorials. There was an immediate connection. The fine attention 
+                    to detail, trail and error process, and puzzle-solving reminded me so much of what it is like to learn music and 
+                    play an instrument. I felt like so many skills translated from music making to coding. I knew from then on 
+                    that I wanted to pursue a career as a software engineer. At the end of my second year of teaching, 
+                    I started to pursue my Bachelor's in Computer Science. THIS IS NOT DONE YET!</p>
             </div>
 
         </div>
